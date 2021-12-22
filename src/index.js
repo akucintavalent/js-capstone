605c3367e37e6bc388bae0fb3520afd632880673
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-12-22 13:37:24 +0100

    Create localStorage variable likes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import logo from './images/logo.png';
 import showPopup from './popup.js';
 import postData from './post-data.js';
 
+const likes = JSON.parse(localStorage.getItem('likes') || '{}');
+
 window.onload = () => {
   document.getElementById('logo').src = logo;
 
@@ -23,10 +25,17 @@ window.onload = () => {
       document.getElementById('items').appendChild(newLiEl);
     });
     hiddenLiEl.style.display = 'none';
-    Array.from(document.getElementsByClassName('like-btn')).forEach((likeBtn) => likeBtn.addEventListener('click', () => {
-      likeBtn.classList.toggle('fas');
-      likeBtn.classList.toggle('far');
-    }));
+    Array.from(document.getElementsByClassName('like-btn')).forEach((likeBtn, index) => {
+      const listener = async () => {
+        likes[index] = true;
+        localStorage.setItem('likes', JSON.stringify(likes));
+        await postData('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/tkXsqcuViZTSE1UznPEd/likes', { item_id: `${index}` });
+        likeBtn.classList.toggle('fas');
+        likeBtn.classList.toggle('far');
+        likeBtn.removeEventListener(listener);
+      };
+      likeBtn.addEventListener('click', listener);
+    });
     const detailBtns = document.querySelectorAll('.item_detail');
     detailBtns.forEach((detail) => {
       detail.addEventListener('click', (e) => {
