9d142d68fbcca309af3353bde6ba37d6dd321853
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-12-22 13:42:52 +0300

    feat: Add html elemens and styles for the comment section

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,8 @@
+/* eslint-disable linebreak-style */
 const popup = document.querySelector('.popup');
 const popupImg = document.getElementById('popup-img');
 const popupTitle = document.getElementById('popup-title');
-const popupIdbm = document.getElementById('popup-idbm');
+const popupImdb = document.getElementById('popup-imdb');
 const popupSummary = document.getElementById('popup-summary');
 const popupClose = document.querySelector('.popup-close');
 
@@ -14,21 +15,22 @@ async function getDetails(item) {
 }
 
 export default async function showDetails(item = 1) {
-  document.body.style.overflow = 'hidden';
+  document.body.classList.add('body-popup-show');
   const data = await getDetails(item);
   const {
     name, image: { medium: image },
-    rating: { average: idbm },
+    rating: { average: imdb },
     summary,
   } = data;
   popup.style.display = 'block';
   popupTitle.innerText = name;
   popupImg.src = image;
-  popupIdbm.innerText = idbm;
+  popupImdb.innerText = imdb;
   popupSummary.innerHTML = summary;
 }
 
 popupClose.addEventListener('click', () => {
+  document.body.classList.remove('body-popup-show');
+
   popup.style.display = 'none';
-  document.body.style.overflow = 'scroll';
 });
