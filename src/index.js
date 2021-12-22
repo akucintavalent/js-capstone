2e5b24d079361a73361e4113c1ebb2fd3c36aaf9
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-12-22 14:08:12 +0100

    Finish functionality for counting likes

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ window.onload = () => {
 
   async function run() {
     const hiddenLiEl = document.querySelector('#items li');
+    let likesCounts = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/tkXsqcuViZTSE1UznPEd/likes');
+    likesCounts = await likesCounts.json();
     let listMovie = await fetch('https://api.tvmaze.com/shows/1/episodes');
     listMovie = await listMovie.json();
     listMovie.forEach((movie, index) => {
@@ -19,20 +21,28 @@ window.onload = () => {
         image: { medium },
       } = movie;
       const newLiEl = hiddenLiEl.cloneNode(true);
+      newLiEl.style.display = 'flex';
       newLiEl.querySelector('.item-image').src = medium;
       newLiEl.querySelector('.name').innerText = name;
       newLiEl.querySelector('.item_detail').id = index + 1;
+      if (likes[index + 1]) {
+        newLiEl.querySelector('.like-btn').classList.toggle('fas');
+        newLiEl.querySelector('.like-btn').classList.toggle('far');
+      }
+      const [likesCount] = likesCounts.filter((el) => el.item_id === `${index + 1}`);
+      newLiEl.querySelector('.likes-count').innerText = likesCount ? likesCount.likes : 0;
       document.getElementById('items').appendChild(newLiEl);
     });
-    hiddenLiEl.style.display = 'none';
     Array.from(document.getElementsByClassName('like-btn')).forEach((likeBtn, index) => {
       const listener = async () => {
+        if (likes[index]) return;
         likes[index] = true;
+        // .likes-count element
+        likeBtn.nextElementSibling.innerText = +likeBtn.nextElementSibling.innerText + 1;
         localStorage.setItem('likes', JSON.stringify(likes));
         await postData('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/tkXsqcuViZTSE1UznPEd/likes', { item_id: `${index}` });
         likeBtn.classList.toggle('fas');
         likeBtn.classList.toggle('far');
-        likeBtn.removeEventListener(listener);
       };
       likeBtn.addEventListener('click', listener);
     });
