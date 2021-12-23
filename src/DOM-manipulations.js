f3838e2b9d2c1ef1e1ac8dd385148023fad34b2d
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-12-23 16:25:03 +0100

    Add items counter

--- src/DOM-manipulations.js
+++ src/DOM-manipulations.js
@@ -11,9 +11,11 @@ const { popupCommentBtnListener } = require('./listeners.js');
 
 export default async function showMoviesOnHomePage() {
   const hiddenLiEl = document.querySelector('#items li');
+  const moviesLink = document.getElementById('movies');
   const moviesListObj = await getListMovie();
   const likesCounts = await getLikeNumber();
 
+  moviesLink.innerText += ` (${moviesListObj.length})`;
   moviesListObj.forEach((movie, index) => {
     const {
       name,
