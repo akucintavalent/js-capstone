7ca48a530b458d5a3dffecab80668415822fac8b
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-12-24 10:01:10 +0100

    Create test for items counter

---
+++ src/__tests__/items-counter.test.js
@@ -0,0 +1,17 @@
+import mountDOM from 'jsdom-mount';
+import { readFileSync } from 'fs';
+import fetch from 'cross-fetch';
+
+import showMoviesOnHomePage from '../DOM-manipulations.js';
+
+const html = readFileSync('src/index.html', 'utf8');
+global.fetch = fetch;
+
+describe('test items counter', () => {
+  test('items counter should be 39', async () => {
+    mountDOM(html);
+    await showMoviesOnHomePage();
+    const moviesLink = document.getElementById('movies');
+    expect(moviesLink.innerText).toContain('(39)');
+  });
+});
